3216089bdc1213bdcb4b8c2d0fdf00964582efa4
Alberto Herrera Vargas <albertoherreravargas@gmail.com>
2021-05-12 21:20:03 +0200

    Adding spare possibility to the game

--- src/game.py
+++ src/game.py
@@ -1,9 +1,24 @@
 class Game:
     def __init__(self):
+        self._strikes = []
         self._score = 0
+        self._spare_bonus = False
 
     def roll(self, knocked_pins):
-        self._score += knocked_pins
+        self._strikes.append(knocked_pins)
 
     def score(self):
+        if len(self._strikes) == 1:
+            return self._strikes[0]
+        frames = zip(*[iter(self._strikes)]*2)
+        for first_strike, second_strike in frames:
+            if self._spare_bonus:
+                self._score += first_strike
+                self._spare_bonus = False
+            self._score += (first_strike + second_strike)
+            if first_strike + second_strike == 10:
+                self._spare_bonus = True
+
         return self._score
+
+
--- src/test_game.py
+++ src/test_game.py
@@ -4,10 +4,22 @@ from src.game import Game
 
 
 class GameTestCase(unittest.TestCase):
-    def test_when_rolling_without_a_then_the_score_is_updated_by_the_number_of_knocked_pins(self):
+    def test_when_rolling_without_a_spare_then_the_score_is_updated_by_the_number_of_knocked_pins(self):
         expected_score = 5
         game = Game()
 
         game.roll(5)
 
         self.assertEquals(game.score(), expected_score, 'Should\'ve set the proper score')
+
+    def test_when_running_a_whole_game_with_a_spare_bonus_frames_then_the_game_score_is_the_correct_one(self):
+        expected_score = 86
+        game = Game()
+
+        for index in range(2):
+            game.roll(5)
+
+        for index in range(18):
+            game.roll(4)
+
+        self.assertEquals(game.score(), expected_score, 'Should\'ve returned the correct score')
