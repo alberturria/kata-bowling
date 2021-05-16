87f963d851db1e46385e2bbacd4bd314bbe97e9c
Alberto Herrera Vargas <albertoherreravargas@gmail.com>
2021-05-16 21:27:25 +0200

    Adding perfect game test case + refactor

--- src/game.py
+++ src/game.py
@@ -9,48 +9,37 @@ class Game:
         self._strikes.append(knocked_pins)
 
     def score(self):
-        if len(self._strikes) == 1:
-            return self._strikes[0]
         index = 0
         while index < len(self._strikes):
-            if self._strike_bonus:
+            if self._is_strike(index):
                 self._apply_strike_bonus(index)
-            if self._spare_bonus:
+            elif self._is_spare(index):
                 self._apply_spare_bonus(index)
-
-            if index < len(self._strikes) - 1 and self._is_strike(index):
-                self._strike_bonus = True
-            elif index < len(self._strikes)-1 and self._is_spare(index):
-                self._spare_bonus = True
-
-            if index < len(self._strikes) - 1 and not self._strike_bonus:
-                self._score += self._strikes[index + 1]
-
-            self._score += self._strikes[index]
+            else:
+                self._score += self._strikes[index]
 
             index = self._get_updated_index(index)
 
         return self._score
 
     def _apply_strike_bonus(self, index):
-        self._score += self._strikes[index] + self._strikes[index + 1]
-        self._strike_bonus = False
+        if len(self._strikes) > index + 2:
+            self._score += self._strikes[index] + self._strikes[index + 1] + self._strikes[index + 2]
 
     def _apply_spare_bonus(self, index):
-        self._score += self._strikes[index]
-        self._spare_bonus = False
+        self._score += self._strikes[index] + self._strikes[index + 1] + self._strikes[index + 2]
 
     def _is_strike(self, index):
         return self._strikes[index] == 10
 
     def _is_spare(self, index):
-        return self._strikes[index] + self._strikes[index + 1] == 10
+        return self._strikes[index] + self._strikes[index + 1] == 10 if len(self._strikes) > index + 1 else False
 
     def _get_updated_index(self, index):
-        if self._strike_bonus:
-            index += 1
-        else:
+        if self._is_spare(index):
             index += 2
+        else:
+            index += 1
 
         return index
 
--- src/test_game.py
+++ src/test_game.py
@@ -4,33 +4,41 @@ from src.game import Game
 
 
 class GameTestCase(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
     def test_when_rolling_without_a_spare_then_the_score_is_updated_by_the_number_of_knocked_pins(self):
         expected_score = 5
-        game = Game()
 
-        game.roll(5)
+        self.game.roll(5)
 
-        self.assertEquals(game.score(), expected_score, 'Should\'ve set the proper score')
+        self.assertEqual(self.game.score(), expected_score, 'Should\'ve set the proper score')
 
     def test_when_running_a_whole_game_with_a_spare_bonus_frames_then_the_game_score_is_the_correct_one(self):
         expected_score = 86
-        game = Game()
 
         for index in range(2):
-            game.roll(5)
+            self.game.roll(5)
 
         for index in range(18):
-            game.roll(4)
+            self.game.roll(4)
 
-        self.assertEquals(game.score(), expected_score, 'Should\'ve returned the correct score')
+        self.assertEqual(self.game.score(), expected_score, 'Should\'ve returned the correct score')
 
     def test_when_running_a_strike_then_the_game_score_is_the_correct_one(self):
         expected_score = 90
-        game = Game()
 
-        game.roll(10)
+        self.game.roll(10)
 
         for index in range(18):
-            game.roll(4)
+            self.game.roll(4)
 
-        self.assertEquals(game.score(), expected_score, 'Should\'ve returned the correct score')
+        self.assertEqual(self.game.score(), expected_score, 'Should\'ve returned the correct score')
+
+    def test_when_running_all_strikes_then_the_correct_score_is_returned(self):
+        expected_score = 300
+
+        for index in range(12):
+            self.game.roll(10)
+
+        self.assertEqual(self.game.score(), expected_score, 'Should\'ve returned the correct score')
